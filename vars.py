23a61dfcaa60a39c92c6989bbaca1db89e02752d
icryptoboss <141033841+icryptoboss@users.noreply.github.com>
2025-06-24 17:35:34 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -6,7 +6,7 @@ from os import environ
 API_ID = int(environ.get("API_ID", "20842776"))
 API_HASH = environ.get("API_HASH", "a5f3a00d014cccf4b1446e9e1a479270")
 BOT_TOKEN = environ.get("BOT_TOKEN", "1631815313:AAEWsWX6cY_gSo5TUzHsFxqVlYhs3_hEtdU")
-OWNER = int(environ.get("OWNER", "5680454765"))
+OWNER = int(environ.get("OWNER", "1391520393"))
 CREDIT = "RAHUL PATEL"
 AUTH_USER = os.environ.get('AUTH_USERS', '1391520393').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
